c02d7832071019173b10ebe5d1701f70f2c46988
victorien <deadvicmangame@gmail.com>
2023-01-09 13:51:48 +0100

    jeu de la taupe

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const holes = $('.hole');
+const scoreBoard = $('.score')
+const moles = $('.mole')
+
+$('#startGame').click( function () {
+    startGame()
+})
+
+
+function startGame() {
+    // Définir le temps maximum de la partie à 10 secondes
+    const gameTime = 10000;
+
+    let score = 0 // Score du joueur
+
+
+    let startTime = setInterval(function (){
+        let hole = holes.eq(Math.floor(Math.random() * holes.length));
+        hole.addClass('up');
+
+        let duration = Math.random() * (1000 - 200) + 200; // relance entre 200ms et 1s
+
+        setTimeout(function() {
+            hole.removeClass('up');
+        }, duration);
+
+        hole.click(function() {
+            // Augmenter le score du joueur
+            score++;
+            // Faire disparaître la taupe
+            $(this).removeClass('up');
+            scoreBoard.text(score)
+        });
+    }, 200);
+
+    setTimeout(function() { // Permet d'arrêter la game
+        clearInterval(startTime);
+        alert("La partie est terminé! ")
+
+    }, gameTime);
+
+}
